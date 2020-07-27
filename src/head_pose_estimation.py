64807f846ecd453033aca21ea6c04c2a187d61e9
MonicaSai7 <monica.sai7@gmail.com>
2020-07-27 14:25:54 +0530

    Check input to predict

--- src/head_pose_estimation.py
+++ src/head_pose_estimation.py
@@ -2,45 +2,71 @@
 This is a sample class for a model. You may choose to use it as-is or make any changes to it.
 This has been provided just to give you an idea of how to structure your model class.
 '''
+import cv2
+import numpy as np
+from openvino.inference_engine import IECore
 
-class Model_X:
+class Headpose_Estimator:
     '''
-    Class for the Face Detection Model.
+    Class for the Headpose Estimation Model.
     '''
     def __init__(self, model_name, device='CPU', extensions=None):
-        '''
-        TODO: Use this to set your instance variables.
-        '''
-        raise NotImplementedError
+        self.plugin = None
+        self.network = None
+        self.input_name = None
+        self.output_name = None
+        self.exec_network = None
+        self.infer_request = None
+        self.model_name = model_name
+        self.device = device
+        self.extensions = extensions
+        self.model_bin = self.model_name.split('.')[0] + '.bin'
+        self.model_xml = model_name
 
     def load_model(self):
-        '''
-        TODO: You will need to complete this method.
-        This method is for loading the model to the device specified by the user.
-        If your model requires any Plugins, this is where you can load them.
-        '''
-        raise NotImplementedError
+        self.plugin = IECore()
+        self.network = self.plugin.read_network(model=self.model_xml, weights=self.model_bin)
 
+        ### Check for supported layers and add any necessary extensions
+        if self.extensions and 'CPU' in self.device:
+            self.plugin.add_extension(self.extensions, self.device)
+        
+        supported_layers = self.plugin.query_network(network=self.network, device_name=self.device)
+        unsupported_layers = [l for l in self.network.layers.keys() if l not in supported_layers]
+
+        if len(unsupported_layers) != 0:
+            print("Unsupported layers found: {}".format(unsupported_layers))
+            print("Check whether extensions are available to add to IECore.")
+            exit(1)
+        
+        self.exec_network = self.plugin.load_network(network=self.network, device_name=self.device, num_requests=1)
+        self.input_name = next(iter(self.network.inputs))
+        self.output_name = next(iter(self.network.outputs))
+        self.input_shape = self.network.inputs[self.input_name].shape
+        self.output_shape = self.network.outputs[self.output_name].shape
+        print("IR successfully loaded into Inference Engine")
+
+        return
+    
     def predict(self, image):
-        '''
-        TODO: You will need to complete this method.
-        This method is meant for running predictions on the input image.
-        '''
-        raise NotImplementedError
+        
+        p_frame = self.preprocess_input(image)
+        outputs = self.exec_network.infer({self.input_name: p_frame})
+        return self.preprocess_output(outputs)
 
     def check_model(self):
-        raise NotImplementedError
+        pass
 
     def preprocess_input(self, image):
-    '''
-    Before feeding the data into the model for inference,
-    you might have to preprocess it. This function is where you can do that.
-    '''
-        raise NotImplementedError
+        
+        p_frame = cv2.resize(image, (self.input_shape[3], self.input_shape[2]))
+        p_frame = p_frame.transpose((2, 0, 1))
+        p_frame = p_frame.reshape(1, *p_frame.shape)
+        return p_frame
 
     def preprocess_output(self, outputs):
-    '''
-    Before feeding the output of this model to the next model,
-    you might have to preprocess the output. This function is where you can do that.
-    '''
-        raise NotImplementedError
+        result = []
+        result.append(outputs['angle_y_fc'].tolist()[0][0])
+        result.append(outputs['angle_p_fc'].tolist()[0][0])
+        result.append(outputs['angle_r_fc'].tolist()[0][0])
+        return result
