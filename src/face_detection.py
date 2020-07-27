ff2ce85e3b7799259f54066b8c95ac68b0e224d4
MonicaSai7 <monica.sai7@gmail.com>
2020-07-27 14:25:24 +0530

    Update preprocess output

--- src/face_detection.py
+++ src/face_detection.py
@@ -2,8 +2,11 @@
 This is a sample class for a model. You may choose to use it as-is or make any changes to it.
 This has been provided just to give you an idea of how to structure your model class.
 '''
+import cv2
+import numpy as np
+from openvino.inference_engine import IECore
 
-class Model_X:
+class Face_Detection:
     '''
     Class for the Face Detection Model.
     '''
@@ -22,7 +25,7 @@ class Model_X:
 
     def load_model(self):
         self.plugin = IECore()
-        self.network = IENetwork(model=self.model_xml, weights=self.model_bin)
+        self.network = self.plugin.read_network(model=self.model_xml, weights=self.model_bin)
 
         ### Check for supported layers and add any necessary extensions
         if self.extensions and 'CPU' in self.device:
@@ -39,38 +42,40 @@ class Model_X:
         self.exec_network = self.plugin.load_network(network=self.network, device_name=self.device, num_requests=1)
         self.input_name = next(iter(self.network.inputs))
         self.output_name = next(iter(self.network.outputs))
+        self.input_shape = self.network.inputs[self.input_name].shape
+        self.output_shape = self.network.outputs[self.output_name].shape
         print("IR successfully loaded into Inference Engine")
 
         return
-
-    def input_shape(self):
-        return self.network.inputs[self.input_blob].shape
-
-    def output_shape(self):
-        return self.network.outputs[self.output_blob].shape
     
-    def predict(self, image):
+    def predict(self, image, prob_threshold):
+        
+        self.threshold = prob_threshold
         p_frame = self.preprocess_input(image)
-        outputs = self.network.infer({self.input_name: p_frame})
+        outputs = self.exec_network.infer({self.input_name: p_frame})
         coords = self.preprocess_output(outputs[self.output_name])
         face_cropped = self.crop_face(coords, image)
+
+        if type(face_cropped) == int:
+            return 0, 0
+
         return face_cropped, coords
 
     def crop_face(self, coords, image):
         if len(coords) == 0:
-            return 0, 0
+            return 0
         
         coords = coords[0]
-        height = image.shape[0]
-        width = image.shape[1]
+        self.h = image.shape[0]
+        self.w = image.shape[1]
 
-        coords = coords* np.array([width, height, width, height])
+        coords = coords* np.array([self.w, self.h, self.w, self.h])
         coords = coords.astype(np.int32)
 
         return image[coords[1]:coords[3], coords[0]:coords[2]]
 
     def check_model(self):
-        raise NotImplementedError
+        pass
 
     def preprocess_input(self, image):
         p_frame = cv2.resize(image, (self.input_shape[3], self.input_shape[2]))
@@ -82,11 +87,11 @@ class Model_X:
         coordinates = []
         for obj in outputs[0][0]:
             ### Draw bounding box if exceeding threshold
+            
             if obj[2] > self.threshold:
-                x_min = int(obj[3] * self.w)
-                y_min = int(obj[4] * self.h)
-                x_max = int(obj[5] * self.w)
-                y_max = int(obj[6] * self.h)
-
+                x_min = obj[3]
+                y_min = obj[4]
+                x_max = obj[5]
+                y_max = obj[6]
                 coordinates.append([x_min, y_min, x_max, y_max])
         return coordinates
